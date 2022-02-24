46e7ec0a824db27295e245b6a6195d549facd484
Jake <yapjakebrian1@gmail.com>
2022-02-24 20:11:11 +0800

    Drop fingerprints
    Signed-off-by: Jake <yapjakebrian1@gmail.com>

--- awaken_pine.mk
+++ awaken_pine.mk
@@ -30,9 +30,3 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys"
